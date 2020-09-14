4edb6bb421bd4b239796c49ef11530c3cd5a2231
Xavier Garcia <xaviergn60@gmail.com>
2020-09-14 05:26:50 -0600

    clase 13 terminada

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
-const messages = ["xavier", "coco", "jorge", "pau", "daniela", "diego"];
+const messages = [
+  "xavier",
+  "coco",
+  "jorge",
+  "pau",
+  "daniela",
+  "diego",
+  "julieta",
+  "desire",
+  "crisly",
+];
 
 const randomMsg = () => {
   const message = messages[Math.floor(Math.random() * messages.length)];
